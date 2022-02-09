b9783b13aff645b9cbb89a105fffb424949d8774
zibbazabba905 <99218211+zibbazabba905@users.noreply.github.com>
2022-02-09 15:14:29 -0600

    Add files via upload

--- debug.js
+++ debug.js
@@ -6,3 +6,34 @@ function debugTest(testThing,time){
     setTimeout(()=>{debugToggle = false}, time)
   }
 }
+
+
+
+//server side
+...
+  socket.on("ping", () => {
+    socket.emit("pong");
+  });
+...
+//client side
+let pingTimerToggle = false;
+let pong = false;
+let pingTime = 0;
+socket.on("pong", () => {
+  pong = true;
+});
+function pingTest() {
+  if (pong) {
+    console.log(Math.round(pingTime * 16.6));
+    pingTime = 0;
+    pong = false;
+  }
+  if (!pingTimerToggle) {
+    socket.emit("ping");
+    setTimeout(() => {
+      pingTimerToggle = false;
+    }, 1000);
+    pingTimerToggle = true;
+  }
+  pingTime++;
+}
